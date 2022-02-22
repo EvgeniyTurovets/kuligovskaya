e11c636a55b66103cce001695961ff1f74b3586c
sanyokstd <sanyokstd@gmail.com>
2022-02-22 18:15:03 +0200

    1
    1

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 $(function(){
+    lazyload();
+
     $(document).on('scroll', function(){
         if(window.pageYOffset > 0){
             $('#header').addClass('fix')
@@ -59,7 +61,7 @@ $(function(){
             slidesPerView: 1,
             loop: true,
             allowTouchMove: true,
-            spaceBetween: 20,
+            spaceBetween: 2000,
         })
     }
     
@@ -74,7 +76,6 @@ $(function(){
             delay: 5000,
         },
         allowTouchMove: false,
-        watchSlidesProgress: true,
 
     })
 
@@ -82,14 +83,71 @@ $(function(){
     const swiperImg = new Swiper('#main-slider-img', {
         lazy: true,
         loop: true,
-       
+        autoplay: {
+            delay: 5000,
+        },
         allowTouchMove: false,
-        breakpoints: {
-            992: {
-                effect: "fade",
-            }
-        }
+        effect: "fade",
     })
 
 
+    // табы на главной
+    var customSwipers = document.querySelectorAll('.custom-swiper')
+    if(customSwipers.length){
+        customSwipers.forEach(function(el, index){
+            var customSwiper = new Swiper(el, {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                pagination: {
+                    el: '.swiper-pagination-custom',
+                    clickable: true
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next-custom',
+                    prevEl: '.swiper-button-prev-custom',
+                },
+                breakpoints: {
+                    575: {
+                        slidesPerView: 'auto',
+                    }
+                }
+            })
+        })        
+    }
+
+    $('.main-tabs__nav li').on('click', function(){
+        if(!$(this).hasClass('active')){
+            $('.main-tabs__nav li').removeClass('active')
+            $(this).addClass('active')
+
+            var index = $(this).index()
+            $('.main-tabs__content').hide()
+            $('.main-tabs__content').eq(index).fadeIn()
+        }
+    })
+
+    $('.more-catalog-show').on('click', function(){
+        $('.main-catalog__bot').addClass('active')
+        $('.main-catalog__wrap--more').slideDown().css('display','flex')
+    })
+
+    $('.more-catalog-hide').on('click', function(){
+        $('.main-catalog__bot').removeClass('active')
+        $('.main-catalog__wrap--more').slideUp()
+    })
+
+    function refreshMainCompany(){
+        if($(window).width() < 992){
+            $('.company__media').appendTo('.company__mob')
+            $('.company__btn').appendTo('.company__mob')
+        }else{
+            $('.company__media').appendTo('.company__left')
+            $('.company__btn').appendTo('.company__right__wrap')
+        }
+    }   
+    refreshMainCompany()
+
+    $(window).on('resize', function(){
+        refreshMainCompany()
+    })
 })
